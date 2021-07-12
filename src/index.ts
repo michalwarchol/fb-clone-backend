2622c9cef78424efc9d26afbe77421b9f75a9a4a
Michał Warchoł <imsimilar@gmail.com>
2021-07-12 17:42:31 +0200

    :gear: Set cors origin to localhost

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ import redis from "redis";
 import session from "express-session";
 import connectRedis from "connect-redis";
 import { MyContext } from "./types";
+import cors from "cors"
 
 const main = async () => {
   const orm = await MikroORM.init(mikroORMConfig);
@@ -22,6 +23,11 @@ const main = async () => {
 
   const RedisStore = connectRedis(session);
   const redisClient = redis.createClient();
+
+  app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+  }))
 
   app.use(
     session({
@@ -47,13 +53,16 @@ const main = async () => {
       resolvers: [HelloResolver, PostResolver, UserResolver],
       validate: false,
     }),
-    context: ({req, res}): MyContext => ({ em: orm.em, req, res }),
+    context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
   });
 
-  apolloServer.applyMiddleware({ app });
+  apolloServer.applyMiddleware({
+    app,
+    cors: false
+  });
 
-  app.listen(3000, () => {
-    console.log("Server started at localhost:3000");
+  app.listen(4000, () => {
+    console.log("Server started at localhost:4000");
   });
 };
 
