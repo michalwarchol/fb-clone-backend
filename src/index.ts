645034b7c9b49274a835f544281840fad3e49995
Michał Warchoł <imsimilar@gmail.com>
2021-07-21 18:00:59 +0200

    :muscle: Cookie name refactored

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "dotenv-safe/config";
 import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
+import { COOKIE_NAME, __prod__ } from "./constants";
 import mikroORMConfig from "./mikro-orm.config";
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
@@ -31,7 +31,7 @@ const main = async () => {
 
   app.use(
     session({
-      name: "qid",
+      name: COOKIE_NAME,
       store: new RedisStore({
         client: redisClient,
         disableTouch: true,
