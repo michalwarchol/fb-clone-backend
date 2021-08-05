55bc41c0ff97aaaa20c7c077c1e340f5b74490dc
Michał Warchoł <imsimilar@gmail.com>
2021-08-05 23:31:23 +0200

    :scroll: Added fields

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,23 +1,75 @@
 import { Field, Int, ObjectType } from "type-graphql";
-import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { User } from "./User";
 
 @ObjectType()
 @Entity()
 export class Post extends BaseEntity {
-
-  @Field(()=>Int)
+  @Field(() => Int)
   @PrimaryGeneratedColumn()
   _id!: number;
 
-  @Field(()=> String)
-  @CreateDateColumn()
-  createdAt: Date;
-  
-  @Field(()=> String)
-  @UpdateDateColumn()
-  updatedAt: Date;
+  @Field()
+  @Column()
+  creatorId: number;
+
+  @Field()
+  @ManyToOne(() => User, (user) => user.posts)
+  creator: User;
 
   @Field()
   @Column()
-  name!: string;
+  text!: string;
+
+  @Field()
+  @Column({ nullable: true, default: null })
+  feeling!: string;
+
+  @Field()
+  @Column({ nullable: true, default: null })
+  activity!: string;
+
+  @Field()
+  @Column({ type: "int", default: 0 })
+  like!: number;
+
+  @Field()
+  @Column({ type: "int", default: 0 })
+  love!: number;
+
+  @Field()
+  @Column({ type: "int", default: 0 })
+  care!: number;
+
+  @Field()
+  @Column({ type: "int", default: 0 })
+  haha!: number;
+
+  @Field()
+  @Column({ type: "int", default: 0 })
+  wow!: number;
+
+  @Field()
+  @Column({ type: "int", default: 0 })
+  sad!: number;
+
+  @Field()
+  @Column({ type: "int", default: 0 })
+  angry!: number;
+
+  @Field(() => String)
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Field(() => String)
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
