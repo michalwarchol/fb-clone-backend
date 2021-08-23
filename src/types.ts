9e1975e1c1d5a43fb88d6fbad8de0c998e5bdd6c
Michał Warchoł <imsimilar@gmail.com>
2021-08-23 22:23:27 +0200

    :three: Added s3 to MyContext

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,4 @@
+import { S3 } from "aws-sdk";
 import {Request, Response} from "express"
 import { Session, SessionData } from "express-session";
 import {Redis} from "ioredis"
@@ -6,4 +7,5 @@ export type MyContext = {
     req: Request & { session: Session & Partial<SessionData> & { userId?: number } }
     redis: Redis
     res: Response
+    s3: S3
 }
