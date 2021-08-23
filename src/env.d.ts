6acadc8c85bb0e69b3d18f131a768a04e79f2f00
Michał Warchoł <imsimilar@gmail.com>
2021-08-23 22:22:19 +0200

    :cactus: Added environmental variables for aws s3

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,9 @@
 declare namespace NodeJS {
     export interface ProcessEnv {
-      POSTGRESQL_PASSWORD: string
+      POSTGRESQL_PASSWORD: string;
+      AWS_ID: string;
+      AWS_SECRET: string;
+      AWS_BUCKET_NAME: string;
+      AWS_REGION: string;
     }
   }
