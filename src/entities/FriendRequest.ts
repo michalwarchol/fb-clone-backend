06b0b3086859e9c64e0aeadb8ecdd614efa59bb7
Michał Warchoł <imsimilar@gmail.com>
2021-08-29 17:31:19 +0200

    :alarm_clock: Added timestamps

--- src/entities/FriendRequest.ts
+++ src/entities/FriendRequest.ts
@@ -1,20 +1,33 @@
 import { Field, ObjectType } from "type-graphql";
-import { BaseEntity, Column, Entity, PrimaryColumn } from "typeorm";
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
 
 @ObjectType()
 @Entity()
 export class FriendRequest extends BaseEntity {
+  @Field(() => String)
+  @Column()
+  status: "accepted" | "in-progress";
 
-    @Field(()=>String)
-    @Column()
-    status: "accepted" | "in-progress";
+  @Field()
+  @PrimaryColumn()
+  sender: number;
 
-    @Field()
-    @PrimaryColumn()
-    sender: number;
+  @Field()
+  @PrimaryColumn()
+  receiver: number;
 
-    @Field()
-    @PrimaryColumn()
-    receiver: number;
+  @Field(() => String)
+  @CreateDateColumn()
+  createdAt: Date;
 
+  @Field(() => String)
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
