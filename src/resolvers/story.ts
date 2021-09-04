ca2cc8287dafbd6fb96190670d20e17193f6366b
Michał Warchoł <imsimilar@gmail.com>
2021-09-04 22:48:18 +0200

    :heart_eyes: Added createStory function

---
+++ src/resolvers/story.ts
@@ -0,0 +1,65 @@
+import { FileUpload, GraphQLUpload } from "graphql-upload";
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  Query,
+  Resolver,
+  UseMiddleware,
+} from "type-graphql";
+import { v4 } from "uuid";
+import { Story } from "../entities/Story";
+import { isAuth } from "../middleware/isAuth";
+import { MyContext } from "../types";
+
+@InputType()
+class StoryInput {
+
+  @Field()
+  text: string;
+
+  @Field()
+  font: string;
+
+  @Field()
+  background: number;
+
+  @Field({nullable: true})
+  time?: number;
+}
+
+@Resolver(Story)
+export class StoryResolver {
+  @Query(() => [Story])
+  async getStories() {
+    return Story.find({});
+  }
+
+  @Mutation(() => Story)
+  @UseMiddleware(isAuth)
+  async createStory(
+    @Ctx() { req, s3 }: MyContext,
+    @Arg("input", () => StoryInput) input: StoryInput,
+    @Arg("image", () => GraphQLUpload, { nullable: true }) image?: FileUpload
+  ) {
+    let imageId = undefined;
+    if (image) {
+      imageId = v4();
+      await s3
+        .upload({
+          Bucket: process.env.AWS_BUCKET_NAME,
+          Key: imageId,
+          Body: image.createReadStream(),
+        })
+        .promise();
+    }
+
+    return Story.create({
+        ...input,
+        userId: req.session.userId,
+        imageId
+    }).save();
+  }
+}
