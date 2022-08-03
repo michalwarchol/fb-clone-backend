daee0f89dcde5964f1895ac27abc166c6aa8b49a
Michał Warchoł <mwarchol33@gmail.com>
2022-08-03 22:43:36 +0200

    Fix versionBumperScript

--- versionBumper.sh
+++ versionBumper.sh
@@ -2,10 +2,10 @@ BRANCH=$(git rev-parse --abbrev-ref HEAD)
 CHANGED=$(git diff-index --name-only HEAD --)
 if [ ! -z "${CHANGED}" ]; then
   echo "Commit your chages before bumping!";
-  return;
+  #return;
 fi;
 
-if [ "${$BRANCH,0,4}" == "feat" ]
+if [ $(echo $INPUT| cut -d'_' -f 2) == "feat" ]
 then
     yarn version --minor;
 else
